b0d9c317cead83741acb2e693baea1edc10256de
LockyWen <2846434229@qq.com>
2020-09-03 20:38:27 -0400

    Set up TinNews project. Add botton view nav

---
+++ app/src/main/java/com/laioffer/tinnews/model/Article.java
@@ -0,0 +1,51 @@
+package com.laioffer.tinnews.model;
+
+import java.util.Objects;
+
+public class Article {
+    public String author;
+    public String content;
+    public String description;
+    public String publishedAt;
+    public String title;
+    public String url;
+    public String urlToImage;
+    public boolean favorite;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Article)) return false;
+        Article article = (Article) o;
+        return favorite == article.favorite &&
+                author.equals(article.author) &&
+                content.equals(article.content) &&
+                description.equals(article.description) &&
+                publishedAt.equals(article.publishedAt) &&
+                title.equals(article.title) &&
+                url.equals(article.url) &&
+                urlToImage.equals(article.urlToImage);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(author, content, description, publishedAt, title, url, urlToImage, favorite);
+    }
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "author='" + author + '\'' +
+                ", content='" + content + '\'' +
+                ", description='" + description + '\'' +
+                ", publishedAt='" + publishedAt + '\'' +
+                ", title='" + title + '\'' +
+                ", url='" + url + '\'' +
+                ", urlToImage='" + urlToImage + '\'' +
+                ", favorite=" + favorite +
+                '}';
+    }
+}
+
+
+
---
+++ app/src/main/java/com/laioffer/tinnews/ui/home/HomeFragment.java
@@ -0,0 +1,47 @@
+package com.laioffer.tinnews.ui.home;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.laioffer.tinnews.R;
+import com.laioffer.tinnews.repository.NewsRepository;
+import com.laioffer.tinnews.repository.NewsViewModelFactory;
+
+public class HomeFragment extends Fragment {
+
+    private HomeViewModel viewModel;
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        NewsRepository repository = new NewsRepository(getContext());
+        viewModel = new ViewModelProvider(this, new NewsViewModelFactory(repository))
+                .get(HomeViewModel.class);
+        viewModel.setCountryInput("us");
+        viewModel
+                .getTopHeadlines()
+                .observe(
+                        getViewLifecycleOwner(),
+                        newsResponse -> {
+                            if (newsResponse != null) {
+                                Log.d("HomeFragment", newsResponse.toString());
+                            }
+                        });
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
